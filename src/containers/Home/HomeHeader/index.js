71a54f07372d8d6f9c607567357a174ed4722622
zhufengnodejs <zhufengnodejs@126.com>
2017-11-14 15:39:54 +0800

    4.编写底部导航的样式

--- src/containers/Home/HomeHeader/index.js
+++ src/containers/Home/HomeHeader/index.js
@@ -2,22 +2,32 @@ import React, {Component} from 'react';
 import './index.less'
 import logo from '../../../images/logo.png';
 export default class HomeHeader extends Component {
+  constructor(){
+    super();
+    this.state = {isShow:false};//是否显示菜单
+  }
+  getMenuList = ()=>(
+    <ul className="menu-list">
+      <li>Node课程培训</li>
+      <li>HTML培训课程</li>
+      <li>视频课程</li>
+      <li>文档课件</li>
+    </ul>
+  )
   render() {
     return (
       <div className="home-header">
         <div className="home-logo">
           <img src={logo}/>
-          <div>
-            <i className="iconfont icon-uilist"/>
-            <i className="iconfont icon-guanbi"/>
+          <div onClick={()=>this.setState({isShow:!this.state.isShow})}>
+            {
+              this.state.isShow?<i className="iconfont icon-guanbi"/>:<i className="iconfont icon-uilist"/>
+            }
           </div>
         </div>
-        <ul className="menu-list">
-          <li>Node课程培训</li>
-          <li>HTML培训课程</li>
-          <li>视频课程</li>
-          <li>文档课件</li>
-        </ul>
+        {
+          this.state.isShow?this.getMenuList():null
+        }
       </div>
     )
   }
