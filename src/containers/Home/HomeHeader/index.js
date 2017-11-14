37c7931ff86d3b279812da8ddd0ebb3f09b95a2b
zhufengnodejs <zhufengnodejs@126.com>
2017-11-14 15:53:53 +0800

    4.编写底部导航的样式

--- src/containers/Home/HomeHeader/index.js
+++ src/containers/Home/HomeHeader/index.js
@@ -1,33 +1,43 @@
 import React, {Component} from 'react';
 import './index.less'
 import logo from '../../../images/logo.png';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 export default class HomeHeader extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {isShow:false};//是否显示菜单
+    this.state = {isShow: false};//是否显示菜单
   }
-  getMenuList = ()=>(
-    <ul className="menu-list">
-      <li>Node课程培训</li>
-      <li>HTML培训课程</li>
-      <li>视频课程</li>
-      <li>文档课件</li>
-    </ul>
+  //当一个元素进入的时候，会先添加fade-enter的类名，然后立马添加fade-enter-active的类名。
+  getMenuList = () => (
+    <CSSTransition
+      timeout={1000}
+      classNames="fade"
+    >
+      <ul className="menu-list">
+        <li>Node课程培训</li>
+        <li>HTML培训课程</li>
+        <li>视频课程</li>
+        <li>文档课件</li>
+      </ul>
+    </CSSTransition>
   )
+
   render() {
     return (
       <div className="home-header">
         <div className="home-logo">
           <img src={logo}/>
-          <div onClick={()=>this.setState({isShow:!this.state.isShow})}>
+          <div onClick={() => this.setState({isShow: !this.state.isShow})}>
             {
-              this.state.isShow?<i className="iconfont icon-guanbi"/>:<i className="iconfont icon-uilist"/>
+              this.state.isShow ? <i className="iconfont icon-guanbi"/> : <i className="iconfont icon-uilist"/>
             }
           </div>
         </div>
-        {
-          this.state.isShow?this.getMenuList():null
-        }
+        <TransitionGroup>
+          {
+            this.state.isShow ? this.getMenuList() : null
+          }
+        </TransitionGroup>
       </div>
     )
   }
