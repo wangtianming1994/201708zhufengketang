1d17c5ff0f5927464c9aceb02a2fc09eeec5bc79
zhufengnodejs <zhufengnodejs@126.com>
2017-11-14 11:54:08 +0800

    2.跑通路由

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  NavLink
+} from 'react-router-dom';
+import './index.less'
+import Home from "../Home/index";
+import Courses from "../Courses/index";
+import Profile from "../Profile/index";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/courses" component={Courses}/>
+          <Route path="/profile" component={Profile}/>
+
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/containers/Courses/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './index.less'
+export default class Courses extends Component {
+  render() {
+    return (
+      <div>
+        Courses
+      </div>
+    )
+  }
+}
