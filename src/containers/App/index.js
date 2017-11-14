d9c8497e19cab69a679d4919e67d6c8e5f04469c
zhufengnodejs <zhufengnodejs@126.com>
2017-11-14 12:21:10 +0800

    3.编写tab组件DOM结构

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,6 +8,7 @@ import './index.less'
 import Home from "../Home/index";
 import Courses from "../Courses/index";
 import Profile from "../Profile/index";
+import Tab from "../../components/Tab/index";
 export default class App extends Component {
   render() {
     return (
@@ -16,7 +17,7 @@ export default class App extends Component {
           <Route exact path="/" component={Home}/>
           <Route path="/courses" component={Courses}/>
           <Route path="/profile" component={Profile}/>
-
+          <Tab/>
         </div>
       </Router>
     )
