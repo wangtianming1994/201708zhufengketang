ef560a870a3a7fb28bd50f3dd88ab0259f56ab67
zhufengnodejs <zhufengnodejs@126.com>
2017-11-15 10:57:41 +0800

    5.轮播图

--- ware/3.promise.js
+++ ware/3.promise.js
@@ -7,16 +7,27 @@ let reducer = (state = initState, action) => {
   console.log(action);
   switch (action.type) {
     case 'ADD':
-      return {number: state.number + 1};
+      return {number: state.number + action.payload};
     default:
       return state;
   }
 }
 let store = createStore(reducer, applyMiddleware(thunk,promise, logger));
 //这是一promise实例，用来返回一个promise
-let task =  new Promise(function (resolve, reject) {
+/*let task =  new Promise(function (resolve, reject) {
   setTimeout(() => {
     resolve({type: 'ADD'});
   }, 1000);
 });
 store.dispatch(task);
+ */
+//第二种用法 payload属性是一个promise
+store.dispatch({
+  type:'ADD',
+  payload:new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("ok");
+    }, 3000);
+  })
+});
+//{type:'ADD',payload:'ok'}
