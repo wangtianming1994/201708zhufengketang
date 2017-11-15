87490b803a7e3ef003b94f1355308eb5a87c9aaf
zhufengnodejs <zhufengnodejs@126.com>
2017-11-15 15:24:00 +0800

    5.轮播图

--- src/containers/Home/Slider/index.js
+++ src/containers/Home/Slider/index.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
+import './index.less'
 import ReactSwipe from 'react-swipe';
 export default class Slider extends Component {
+  constructor(){
+    super();
+    this.state = {index:0};
+  }
   render() {
     let swipeOptions = {
       continuous:true,
       auto:600,
-      callback(index){
-        console.log(index);
+      callback:(index)=>{
+        this.setState({index});
       }
     }
     return (
@@ -22,6 +27,13 @@ export default class Slider extends Component {
         }
           </ReactSwipe>:null
         }
+        <div className="dots">
+          {
+            this.props.sliders.map((item,index)=>(
+              <span key={index} className={this.state.index == index?"active":""}></span>
+            ))
+          }
+        </div>
       </div>
     )
   }
