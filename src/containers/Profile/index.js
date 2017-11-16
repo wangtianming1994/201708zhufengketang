b2aa16b6d0f522582cb70bbecb7a72e9107b6721
zhufengnodejs <zhufengnodejs@126.com>
2017-11-16 17:27:54 +0800

    6.下拉刷新

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import './index.less'
 import {Link} from 'react-router-dom';
-export default class Profile extends Component {
+import {connect} from 'react-redux';
+class Profile extends Component {
   render() {
     return (
       <div className="profile">
         <div className="profile-bg">
           <img src={require('../../images/profile.png')} alt=""/>
-          <Link to="/login">登录</Link>
+          <Link to="/login">{this.props.user?this.props.user.username:"登录"}</Link>
         </div>
       </div>
     )
   }
 }
+export default connect(
+  state=>state.session
+)(Profile);
